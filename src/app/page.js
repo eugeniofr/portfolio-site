72788ce95d1b7efde67838f40da8adc2a63a2557
Eugenio Felix <efeli24@wgu.edu>
2026-04-01 21:55:22 -0600

    Changed h1 to extra bold

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-zinc-50 px-4">
 			<div className="text-center space-y-6">
-				<h1 className="text-5xl md:text-7xl font-display">Eugenio Felix</h1>
+				<h1 className="text-5xl md:text-7xl font-display font-extrabold">Eugenio Felix</h1>
 				<p className="text-lg md:text-xl max-w-lg mx-auto font-sans">Graphic Designer & Software Engineer</p>
 
 				<div className="pt-12">
